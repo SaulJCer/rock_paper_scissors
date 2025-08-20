483bfa0a750c92dab20f7722f9bc4ce725fa68d7
SaulJCer <190316056+SaulJCer@users.noreply.github.com>
2025-08-20 15:02:02 -0400

    added play round functions
    need to fix the player winning conditions it has a bug and does not show player winning when player wins

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # rock_paper_scissors
 make a rock paper scissors game so player can play against cpu
+
+<!-- assign rock to value of 0
+assinn paper to value of 1
+assign scissors to value of 2 -->
+
+function that gets random computer choice
+    line using math.random floor to get a value of 0, 1 or 2
+
+    check for if value = 0 return value of rock
+
+    check for value = 1 return value of paper
+
+    check for value = 2 return value of scissors
+
+
+function to that gets players choice
+ask user for input
+
+play round function 
+compare choices 
+check if equal - which return a tie
+check conditons 
+    player - rock -- cpu paper
+    player - rock -- cpu scissors
+    player - paper -- scissors
+    
+    rock, rock
+    paper, paper
+    sci, sci
+
+    condtion player wins 
+    rock, sci -- paper, rock -- sci, sci -- paper
+    cond cpu wins
+    rock -- paper,  sci -- rock, paper -- sci
+
+
+    rock, paper - paper, rock
+    rock, sci - sci, rock
+    paper, sci - sci, paper
+    
--- game.js
+++ game.js
@@ -23,20 +23,39 @@ function getComputerChoice() {
 }
 console.log(getComputerChoice());
 
-// console.log(Math.random() * 3);
+console.log(Math.random() * 3);
 
-// function getPlayerChoice() {
-//     return prompt("Rock, Paper or Scissors");
-// }
+function getPlayerChoice() {
+    return prompt("Rock, Paper or Scissors");
+}
 
-// // console.log(getPlayerChoice());
+console.log(getPlayerChoice());
 
-// function playRound() {
-//     let playerChoice = getPlayerChoice();
-//     let computerChoice = getComputerChoice();
-//     if (playerChoice == computerChoice) {
-//         return console.log('Its a tie')
-//     }
-//     else if ((playerChoice == "Rock")    )
-// }
+function playRound() {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    if (playerChoice == computerChoice) {
+        return console.log('Its a tie')
+    }
+    else if (
+        ((playerChoice == rock) && (computerChoice == scissors))
+        || ((playerChoice == paper) && (computerChoice == rock))
+        || ((playerChoice == scissors) && (computerChoice == paper))
+    ) {
+        humanScore += 1;
+        return console.log("You won Score: You: " + humanScore + " CPU: " + cpuScore);
+    }
+    // 2nd else if could just be an else, but 
+    // since using string values, we will make sure
+    else if (
+        ((playerChoice == rock) && (computerChoice == paper))
+        || ((playerChoice == paper) && (computerChoice == scissors))
+        || ((playerChoice == scissors) && (computerChoice == rock))
+    ){
+        cpuScore += 1;
+        return console.log("You lost Score: You: " + humanScore + " CPU: " + cpuScore);
+    }
+}
+
+playRound();
 
