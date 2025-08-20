b7939e4fa53438bf6f67540a46929da636ec4483
SaulJCer <190316056+SaulJCer@users.noreply.github.com>
2025-08-20 14:25:27 -0400

    Made function for getting computer game value

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
 console.log("Hola world")
+
+
+
+function getComputerChoice() {
+    cpuChoice = Math.floor(Math.random() * 3); // gets a number from 0 - 2 inclusive
+
+    if (cpuChoice == 0) {
+        return "Rock"
+    }
+    else if (cpuChoice == 1) {
+        return "Paper";
+    }
+    else if (cpuChoice == 2) {
+        return "Scissors";
+    }
+
+}
+console.log(getComputerChoice());
+
+// console.log(Math.random() * 3);
