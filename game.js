6a2395dbfaf553ddaabb26d438d99fec52a73ae8
SaulJCer <190316056+SaulJCer@users.noreply.github.com>
2025-08-20 14:44:56 -0400

    started playRound function and added var for choices

--- game.js
+++ game.js
@@ -1,27 +1,42 @@
 console.log("Hola world")
 
+let humanScore = 0;
+let cpuScore = 0;
+let rock = "Rock"
+let paper = "Paper"
+let scissors = "Scissors"
 
 
 function getComputerChoice() {
     cpuChoice = Math.floor(Math.random() * 3); // gets a number from 0 - 2 inclusive
 
     if (cpuChoice == 0) {
-        return "Rock"
+        return rock
     }
     else if (cpuChoice == 1) {
-        return "Paper";
+        return paper;
     }
     else if (cpuChoice == 2) {
-        return "Scissors";
+        return scissors;
     }
 
 }
-// console.log(getComputerChoice());
+console.log(getComputerChoice());
 
 // console.log(Math.random() * 3);
 
-function getPlayerChoice() {
-    return prompt("Rock, Paper or Scissors");
-}
+// function getPlayerChoice() {
+//     return prompt("Rock, Paper or Scissors");
+// }
 
-console.log(getPlayerChoice());
+// // console.log(getPlayerChoice());
+
+// function playRound() {
+//     let playerChoice = getPlayerChoice();
+//     let computerChoice = getComputerChoice();
+//     if (playerChoice == computerChoice) {
+//         return console.log('Its a tie')
+//     }
+//     else if ((playerChoice == "Rock")    )
+// }
+
